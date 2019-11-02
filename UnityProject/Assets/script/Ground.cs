d3d754d5a526db1d216fe04470102c52a5afc929
jack1019520 <jack10091019520>
2019-11-02 20:58:42 +0800

    完成場景與介面和新增地板腳本

---
+++ UnityProject/Assets/script/Ground.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    //宣告整數變數(定義欄位 field)
+    //修飾詞 欄位類型 欄位名稱 (指定 值)結束
+    //private 私人(隱藏)  public 公開 (顯示)
+    //int 整數
+}
